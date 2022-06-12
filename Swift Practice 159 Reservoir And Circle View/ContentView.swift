07dd4abb49e57715b374328148c9cdb5be4f67c1
Dogpa <dogpa0130@gmail.com>
2022-06-12 12:44:33 +0800

    Initial Commit

---
+++ Swift Practice 159 Reservoir And Circle View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 159 Reservoir And Circle View
+//
+//  Created by Dogpa's MBAir M1 on 2022/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 159 Reservoir And Circle View/Swift_Practice_159_Reservoir_And_Circle_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_159_Reservoir_And_Circle_ViewApp.swift
+//  Swift Practice 159 Reservoir And Circle View
+//
+//  Created by Dogpa's MBAir M1 on 2022/6/12.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_159_Reservoir_And_Circle_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
